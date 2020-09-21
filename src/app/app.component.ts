70977d5af1f355105eb932b5a78cc77818bb56f3
Lawrence Horton <lhorton@healthesystems.com>
2020-09-21 12:42:16 -0400

    added step2 solutions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
-import {Component, ViewEncapsulation} from '@angular/core';
-import {CatFactService} from './cat-fact.service';
-import {Observable} from 'rxjs';
-import {CatFactTableItem, SelectOption, User} from './cat-fact.models';
-import {map} from 'rxjs/operators';
-import {uniqBy} from 'lodash';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { CatFactService } from './cat-fact.service';
+import { Observable } from 'rxjs';
+import { CatFactTableItem, SelectOption, User } from './cat-fact.models';
+import { map } from 'rxjs/operators';
+import { uniqBy } from 'lodash';
 
 @Component({
   selector: 'app-root',
@@ -12,34 +12,27 @@ import {uniqBy} from 'lodash';
   encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
-  /**
-   * TODO: Step-2 Pt.2
-   *  Solution
-   *
-   *  @example
-   *
-   *  ``` typescript
-   *  userOptions$: Observable<
-   *  SelectOption<string>[]
-   *  > = this.catFactService.getUsers().pipe(
-   *  map((users: User[]) =>
-   *  // Only get unique users by id
-   *  uniqBy(users, '_id')
-   *  // Filter out undefined
-   *  .filter((user) => !!user)
-   *  // Convert to Select Option
-   *  .map((user) => ({
-   *         label: `${user.name.first} ${user.name.last}`,
-   *         value: user._id,
-   *       })))
-   *  );
-   *  ```
-   */
+  userOptions$: Observable<
+    SelectOption<string>[]
+  > = this.catFactService.getUsers().pipe(
+    map((users: User[]) =>
+      // Only get unique users by id
+      uniqBy(users, '_id')
+        // Filter out undefined
+        .filter((user) => !!user)
+        // Convert to Select Option
+        .map((user) => ({
+          label: `${user.name.first} ${user.name.last}`,
+          value: user._id,
+        }))
+    )
+  );
 
-  tableData$: Observable<CatFactTableItem[]> = this.catFactService.getAllFacts();
+  tableData$: Observable<
+    CatFactTableItem[]
+  > = this.catFactService.getAllFacts();
 
-  constructor(public catFactService: CatFactService) {
-  }
+  constructor(public catFactService: CatFactService) {}
 
   /**
    * TODO: Step-3
